1bb28357b74d770b438952ff14e2653717486cf3
gsbarrera1 <gbarrera@portofsandiego.org>
2018-12-27 12:07:26 -0800

    added browse button

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Button, Label, BOTH
+from tkinter import Tk, Button, Label, filedialog, BOTH, StringVar
 from tkinter.ttk import Frame
 
 class gui(Frame):
@@ -10,6 +10,8 @@ class gui(Frame):
         self.master.title("Revenue Predictions")
         self.pack(fill=BOTH,expand=1)
         self.centerWindow()
+        self.folderButton = Button(self.master,text="Browse", command=browse_button)
+        self.folderButton.pack()
 
     def centerWindow(self):
         w = 290
@@ -21,6 +23,13 @@ class gui(Frame):
         x = (sw - w)/2
         y = (sh - h)/2
         self.master.geometry('%dx%d+%d+%d' % (w,h,x,y))
+
+def browse_button():
+    global folder_path
+    folder_path = StringVar()
+    filename = filedialog.askdirectory()
+    folder_path.set(filename)
+    print(filename)
 
 def main():
     root = Tk()
