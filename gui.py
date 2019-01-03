e384b02c756b067ad1455598ab2b02c99a243adf
gsbarrera1 <gbarrera@portofsandiego.org>
2019-01-02 16:10:03 -0800

    added input line and cwd

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Button, Label, filedialog, StringVar, BOTH, BOTTOM, LEFT, RIGHT, TOP
+from tkinter import Tk, Button, Label, filedialog, Entry, StringVar, BOTH, BOTTOM, LEFT, RIGHT, TOP
 from tkinter.ttk import Frame
 from data_transformer import reformat
 from forecaster import HoltWinters
@@ -8,8 +8,8 @@ class gui(Frame):
 
     def __init__(self):
         super().__init__()
-        folder_path = os.getcwd()
-        Label(self.master,text=folder_path).pack(side = TOP)
+        # folder_path = os.getcwd()
+        # Label(self.master,text=folder_path).pack(side = TOP)
         self.initUI()
 
     def initUI(self):
@@ -17,21 +17,35 @@ class gui(Frame):
         self.pack(fill=BOTH,expand=1)
         self.centerWindow()
 
-        #Predict
+        folder_path = StringVar()
+        folder_path.set(os.getcwd())
+
+        # Current Folder Label
+        m = Label(self.master, text="Directory: ")
+        m.pack(side=TOP)
+
+        l = Label(self.master, textvariable = folder_path)
+        l.pack(side=TOP)
+
+        # Path Input
+        t = Entry(self.master, width=35, textvariable = folder_path)
+        t.pack()
+
+        # Predict
         self.predictButton = Button(self.master,text="Predict", command=predict)
         self.predictButton.pack(side=RIGHT)
 
-        #Browse Folder
+        # Browse Folder
         self.folderButton = Button(self.master,text="Browse", command=browse_button)
         self.folderButton.pack(side=RIGHT)
 
-        #Quit Program
+        # Quit Program
         self.closeButton = Button(self.master, text="Quit", command=self.master.quit)
         self.closeButton.pack(side=LEFT)
 
     def centerWindow(self):
-        w = 600
-        h = 200
+        w = 475
+        h = 100
 
         sw = self.master.winfo_screenwidth()
         sh = self.master.winfo_screenheight()
@@ -43,9 +57,9 @@ class gui(Frame):
 def browse_button():
     global folder_path
     folder_path = StringVar()
-    filename = filedialog.askopenfilename()
+    filename = filedialog.askopenfilename(initialdir='',title="Select File", filetypes=(("CSV Files","*.csv"),("All Files","*.*")))
     folder_path = filename
-    print(filename)
+    print(folder_path)
 
 def predict():
     #Format the data
